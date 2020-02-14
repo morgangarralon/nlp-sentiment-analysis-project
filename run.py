7e482e6ec881770bca0e4205ebba64e5500446f1
momomo <morgan.garralon@gmail.com>
2020-02-14 07:49:48 +0100

    csfr error

--- run.py
+++ run.py
@@ -4,7 +4,8 @@ from flask_wtf.csrf import CSRFProtect
 from app.controllers import MainController, TrainController, GuessController, ApiController
 
 app.config['SECRET_KEY'] = os.urandom(32)
-csrf = CSRFProtect(app)
+csrf = CSRFProtect()
+csrf.init_app(app)
 
 if __name__ == "__main__":
     app.run()
