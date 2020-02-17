52ced4e2c236239e10f1bb099e5ab45d0a223994
momomo <morgan.garralon@gmail.com>
2020-02-17 16:26:04 +0100

    for 6 in case of error (ajaxouenet)

--- app/static/js/forms.js
+++ app/static/js/forms.js
@@ -1,4 +1,31 @@
+function submitForm() { 
+    $.ajax({
+        url: '/result',
+        data: $('form').serialize(),
+        type: 'POST',
+        success: function(response) {
+            document.getElementById("result").innerHTML= "result: " + response;
+        },
+        error: function(error) {
+            console.log(error);
+            $.ajax({
+                url: '/result',
+                data: $('form').serialize(),
+                type: 'POST',
+                success: function(response) {
+                    console.log(response);
+                    document.getElementById("result").innerHTML= "result: " + response;
+                },
+                error: function(error) {
+                    console.log(error);
+                }
+            });
+        }
+    });
+ };
+
 // userInputForm
+
 $(document).ready(function() {
     console.log('total');
     $('#submit').click(function() {
@@ -7,12 +34,14 @@ $(document).ready(function() {
             data: $('form').serialize(),
             type: 'POST',
             success: function(response) {
-                console.log(response);
                 document.getElementById("result").innerHTML= "result: " + response;
             },
             error: function(error) {
                 console.log(error);
+                for(i=0; i<5; i++) {
+                    submitForm()
+                }
             }
-        });
-    });
-});
+        })
+    })
+})
