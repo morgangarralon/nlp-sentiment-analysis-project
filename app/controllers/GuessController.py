51f847ead553b02d5e5de40668df6f003201888a
momomo <morgan.garralon@gmail.com>
2020-02-17 21:09:10 +0100

    ajax done (userInputForm)

--- app/controllers/GuessController.py
+++ app/controllers/GuessController.py
@@ -2,25 +2,25 @@ from app import app
 from app.models.Guesser import Guesser
 from sklearn.model_selection import cross_val_score
 from app.forms.DataUserInputForm import DataUserInputForm
-from flask import render_template, redirect, request, url_for, flash
+from flask import render_template, redirect, request, session, url_for, flash, json
 
 @app.route('/guess/<string:type>', methods=['GET'])
-def guess(type, positive=None):
+def guess(type):
     template = 'error'
     if type == "user":
         form_user = DataUserInputForm()
-        template = render_template('userInputForm.html', form=form_user, positive=positive)
+        guesser = Guesser()
+        guesser.loadGuesser('LogisticRegression.model')
+        model = guesser.getModel()
+        # session['guesser'] = json.dumps(guesser.__dict__)
+        template = render_template('userInputForm.html', form=form_user, model_chooser_info = {
+                                                                    'name': model.__class__.__name__
+                                                                    # 'score': score
+                                                                })
         if form_user.validate_on_submit():
-            guesser = Guesser()
-            guesser.loadGuesser('LogisticRegression.model')
-            model = guesser.getModel()
             flash('{}{}'.format(guesser.getGuess(request.form.get('field_data_input')), request.form.get('field_data_input')))
             # return redirect(url_for('guess', type='user'))
             # score = cross_val_score(model, x, y, scoring='accuracy', cv=10).mean()
-            template = render_template('userInputForm.html', form=form_user, positive=positive, model_chooser_info = {
-                                                                                                'name': type(model).__name__,
-                                                                                                # 'score': score
-                                                                                            })
     elif type == "twitter":
         template ='twitter'
     else:
@@ -30,9 +30,14 @@ def guess(type, positive=None):
 
 @app.route('/result', methods=['POST'])
 def getResult():
+    # guesser = json.loads(session['guesser'])
     guesser = Guesser()
     guesser.loadGuesser('LogisticRegression.model')
-    list_result = [guesser.getGuess(request.form.get('field_data_input')), request.form.get('field_data_input')]
-    result = ' '.join([str(elem) for elem in list_result])
+
+    if guesser is None:
+        result = "ERROR"
+    else:
+        list_result = [guesser.getGuess(request.form.get('field_data_input')), request.form.get('field_data_input')]
+        result = ' '.join([str(elem) for elem in list_result])
 
     return result
--- app/static/js/forms.js
+++ app/static/js/forms.js
@@ -1,10 +1,16 @@
-function submitForm() { 
+function printInDiv(id, input_html) {
+    document.getElementById(id).innerHTML= input_html;
+}
+
+function submitForm() {
+    form = $('form')
+    
     $.ajax({
         url: '/result',
-        data: $('form').serialize(),
+        data: form.serialize(),
         type: 'POST',
         success: function(response) {
-            document.getElementById("result").innerHTML= "result: " + response;
+            printInDiv("result", "result: " + response)
         },
         error: function(error) {
             console.log(error);
@@ -13,35 +19,58 @@ function submitForm() {
                 data: $('form').serialize(),
                 type: 'POST',
                 success: function(response) {
-                    console.log(response);
+                    printInDiv("result", "result: " + response)
+
+                    return true;
+                },
+                error: function(error) {
+                    console.log(error);
+
+                    return false;
+                }
+            });
+        }
+    }); 
+}
+
+// userInputForm
+$(document).ready(function() {
+    console.log('total');
+    $(document).on("keydown", "form", function(event) { 
+        if (event.key == "Enter") {
+            i = 0
+            result = false;
+            while(!result & i < 20) {
+                result = submitForm();
+                i++;
+            }
+            event.preventDefault();
+        }
+    });
+    $('#submit').click(function() {
+        form = $('form')
+        input_guess = form.find("#input-guess #field_data_input").val()
+
+        if(input_guess.length > 2) {
+            $.ajax({
+                url: '/result',
+                data: $('form').serialize(),
+                type: 'POST',
+                success: function(response) {
                     document.getElementById("result").innerHTML= "result: " + response;
                 },
                 error: function(error) {
                     console.log(error);
+                    i = 0
+                    result = false;
+                    while(!result & i < 20) {
+                        result = submitForm();
+                        i++;
+                    }
                 }
-            });
+            })
+        } else {
+            printInDiv("result", "please, type a lengther word (>2)")
         }
-    });
- };
-
-// userInputForm
-
-$(document).ready(function() {
-    console.log('total');
-    $('#submit').click(function() {
-        $.ajax({
-            url: '/result',
-            data: $('form').serialize(),
-            type: 'POST',
-            success: function(response) {
-                document.getElementById("result").innerHTML= "result: " + response;
-            },
-            error: function(error) {
-                console.log(error);
-                for(i=0; i<5; i++) {
-                    submitForm()
-                }
-            }
-        })
     })
 })
